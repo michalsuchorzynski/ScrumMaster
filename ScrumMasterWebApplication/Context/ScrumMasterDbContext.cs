7df4fbab7a59750b789a23b61721e4fab5bd2be6
Michal Suchorzynski <michal.suchorzynski@gmail.com>
2018-11-02 16:58:57 +0100

    Add Identity

---
+++ ScrumMasterWebApplication/Context/ScrumMasterDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumMasterWebApplication.Context
+{
+    public class ScrumMasterDbContext :IdentityDbContext
+    {
+        public ScrumMasterDbContext(DbContextOptions<ScrumMasterDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- ScrumMasterWebApplication/Startup.cs
+++ ScrumMasterWebApplication/Startup.cs
@@ -7,10 +7,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ScrumMasterDataModel;
+using ScrumMasterWebApplication.Context;
 
 namespace ScrumMasterWebApplication
 {
@@ -24,8 +28,14 @@ namespace ScrumMasterWebApplication
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             ConfigureLocalization(services);
+
+            services.AddDbContext<ScrumMasterDbContext>(options => options.UseSqlServer(Configuration["Data:ScrumMaster:ConnectionString"]));
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<ScrumMasterDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -47,6 +57,8 @@ namespace ScrumMasterWebApplication
             //Localization
             app.UseRequestLocalization();
 
+            app.UseAuthentication();
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
